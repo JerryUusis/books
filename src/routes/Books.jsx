c2776dfc7d39a55e93a20c6c8ea0ea115a7e593d
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-19 14:59:25 +0200

    add a placeholder image if url not provided when adding a new book

--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -41,62 +41,59 @@ function Books({ search }) {
             useFlexGap
             flexWrap="wrap"
           >
-            {data.filter((item) => { return (
-              item.name.toLowerCase().includes(search) ||
-              item.author.toLowerCase().includes(search))
-              
-            })
+            {data.filter((item) => {
+              return (
+                item.name.toLowerCase().includes(search) ||
+                item.author.toLowerCase().includes(search))
 
-
-
-              .map((book) => (
-                <Card
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '15%',
-                    minWidth: 200,
-                  }}
-                  key={book.name}
-                >
-                  <CardMedia
-                    sx={{ height: 250 }}
-                    image={book.img}
-                    title={book.name}
-                  />
-                  <Box sx={{ pt: 2, pl: 2 }}>
-                    {book.genres.map((genre, i) => (
-                      <Chip
-                        key={i}
-                        label={genre}
-                        variant="outlined"
-                        size="small"
-                      />
-                    ))}
-                    <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
-                      {book.name}
-                    </Typography>
-                    <Typography variant="subtitle1" gutterBottom>
-                      {book.author}
-                    </Typography>
-                  </Box>
-                  <CardActions
-                    sx={{
-                      justifyContent: 'space-between',
-                      mt: 'auto',
-                      pl: 2,
-                    }}
-                  >
-                    <Rating
-                      name="read-only"
-                      value={book.stars}
-                      readOnly
+            }).map((book) => (
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '15%',
+                  minWidth: 200,
+                }}
+                key={book.name}
+              >
+                <CardMedia
+                  sx={{ height: 250 }}
+                  image={book.img ? book.img : 'https://images.unsplash.com/photo-1621944190310-e3cca1564bd7?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+                  title={book.name}
+                />
+                <Box sx={{ pt: 2, pl: 2 }}>
+                  {book.genres.map((genre, i) => (
+                    <Chip
+                      key={i}
+                      label={genre}
+                      variant="outlined"
                       size="small"
                     />
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              ))}
+                  ))}
+                  <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
+                    {book.name}
+                  </Typography>
+                  <Typography variant="subtitle1" gutterBottom>
+                    {book.author}
+                  </Typography>
+                </Box>
+                <CardActions
+                  sx={{
+                    justifyContent: 'space-between',
+                    mt: 'auto',
+                    pl: 2,
+                  }}
+                >
+                  <Rating
+                    name="read-only"
+                    value={book.stars}
+                    readOnly
+                    size="small"
+                  />
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            ))}
           </Stack>
         </div>
       )}
