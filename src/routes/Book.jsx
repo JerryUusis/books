30766f19a3345f117870e6e565d37f004a3e88de
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-20 12:06:25 +0200

    import Image, add initial styling

--- src/routes/Book.jsx
+++ src/routes/Book.jsx
@@ -6,11 +6,13 @@ import {
   CardActions,
   CardMedia,
   Button,
+
   Stack,
   Rating,
   Chip,
   Typography,
 } from "@mui/material";
+import Image from "mui-image";
 
 function Book({ data, loading }) {
   const params = useParams();
@@ -21,62 +23,52 @@ function Book({ data, loading }) {
 
   return (
     <>
-      <Box sx={{ mx: "auto", p: 2 }}>
-        <div>
+      <Stack 
+        direction="row"
+        >
+        <Image
+          src={book.img}
+          fit="contain"
+          width="50%"
+          sx={{ maxWidth: "400px" }}
+        ></Image>
           <Stack
-            sx={{ justifyContent: "space-around" }}
+            sx={{ 
+              justifyContent: "center",
+              alignItems: "center" }}
             spacing={{ xs: 1 }}
             direction="row"
             useFlexGap
             flexWrap="wrap"
           >
-            <Card
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                width: "15%",
-                minWidth: 400,
-              }}
-              key={book.name}
-            >
-              <CardMedia
-                sx={{ height: 350 }}
-                image={
-                  book.img
-                    ? book.img
-                    : "https://images.unsplash.com/photo-1621944190310-e3cca1564bd7?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                }
-                title={book.name}
-              />
-              <Box sx={{ pt: 2, pl: 2 }}>
-                {book.genres.map((genre, i) => (
-                  <Chip key={i} label={genre} variant="outlined" size="small" />
-                ))}
-                <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
-                  {book.name}
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                  {book.author}
-                </Typography>
-                <Typography variant="subtitle2" gutterBottom>
-                  Completed {book.completed ? "✅" : "❌"}
-                </Typography>
-                {book.start && (
-                  <>
-                    <div>
-                      <Typography variant="subtitle3" gutterBottom>
-                        Starting day {book.start}
-                      </Typography>
-                    </div>
-                    <div>
-                      <Typography variant="subtitle3" gutterBottom>
-                        Ending day {book.end}
-                      </Typography>
-                    </div>
-                  </>
-                )}
-              </Box>
-              <CardActions
+            <Box sx={{ pt: 2, pl: 2 }}>
+              {book.genres.map((genre, i) => (
+                <Chip key={i} label={genre} variant="outlined" size="small" />
+              ))}
+              <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
+                {book.name}
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom>
+                {book.author}
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom>
+                Completed {book.completed ? "✅" : "❌"}
+              </Typography>
+              {book.start && (
+                <>
+                  <div>
+                    <Typography variant="subtitle3" gutterBottom>
+                      Starting day {book.start}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography variant="subtitle3" gutterBottom>
+                      Ending day {book.end}
+                    </Typography>
+                  </div>
+                </>
+              )}
+              <Box
                 sx={{
                   justifyContent: "space-between",
                   mt: "auto",
@@ -89,11 +81,10 @@ function Book({ data, loading }) {
                   readOnly
                   size="small"
                 />
-              </CardActions>
-            </Card>
+              </Box>
+            </Box>
           </Stack>
-        </div>
-      </Box>
+      </Stack>
     </>
   );
 }
